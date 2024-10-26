663ee2cd874a9a4257a541d0bc3f61197d64ea38
mabozed <mabozed.com@gmail.com>
2024-10-26 21:21:39 +0300

    edit Domin

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,6 @@
 export const POSTS_PER_PAGE = 6
 
-const PRODUCTION_DOMAIN = 'https://vica-next-course.vercel.app/'
+const PRODUCTION_DOMAIN = 'https://vica-post-api.vercel.app/'
 const DEVELOPMENT_DOMAIN = 'http://localhost:3000'
 
 export const DOMAIN =
